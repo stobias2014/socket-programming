af3f3678153834f1654a31ccbb1d0b2accac32d8
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-26 20:37:02 -0500

    added run method

--- greeting-server/src/com/tobias/saul/GreetingServer.java
+++ greeting-server/src/com/tobias/saul/GreetingServer.java
@@ -1,11 +1,25 @@
 package com.tobias.saul;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
 
-public class GreetingServer {
+public class GreetingServer extends Thread{
 	
 	private ServerSocket serverSocket;
+	
+	public static void main(String[] args) {
+		int port = 6666;
+		try {
+			Thread t = new GreetingServer(port);
+			t.start();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	public GreetingServer(int port) throws IOException {
 		serverSocket = new ServerSocket(port);
@@ -13,6 +27,24 @@ public class GreetingServer {
 	}
 	
 	public void run() {
+		
+		while(true) {
+			try {
+				System.out.println("Waiting for client...");
+				Socket server = serverSocket.accept();
+				System.out.println("Just connected...");
+				DataInputStream in = new DataInputStream(server.getInputStream());
+				DataOutputStream out = new DataOutputStream(server.getOutputStream());
+				server.close();
+			
+			
+			} catch(SocketTimeoutException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		
 	}
 
